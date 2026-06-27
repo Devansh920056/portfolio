6d19db9416ca2ccef5e2d7302e3f6904e9151888
Devansh920056 <devanshzode2005@gamil.com>
2026-06-27 18:08:25 +0530

    Fixed Sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://devanshzode.com',
+      url: 'https://devanshzode.dev',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
